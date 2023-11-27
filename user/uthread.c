70c59b2daf197500af8790ede04aaffb1823d09d
LithiumTins <1048467185@qq.com>
2023-11-27 16:28:34 +0800

    finish

--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,27 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct reg {
+  uint64 sp;
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+  uint64 ra;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct reg reg;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -60,6 +77,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64) &t->reg, (uint64) &next_thread->reg);
   } else
     next_thread = 0;
 }
@@ -74,6 +92,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->reg.ra = (uint64) func;
+  t->reg.sp = (uint64) &t->stack[STACK_SIZE];
 }
 
 void 
